2ce49a93ce88326dfef51581cb3869bbc1e223c7
Anna Karolyna <129415573+annaa-ps@users.noreply.github.com>
2024-10-27 12:49:56 -0300

    Add files via upload

---
+++ Códigos de Conteúdo/oop/src/Guitar.java
@@ -0,0 +1,10 @@
+public class Guitar {
+    //Propriedades
+    public String name;
+    public int chords;
+
+    //Métodos (Comportamentos)
+    public void play(){
+        // aqui vai ser tocado o audio
+    }
+}
---
+++ Códigos de Conteúdo/oop/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Guitar fender = new Guitar();
+
+        fender.chords = 6;
+        fender.name = "Fender";
+
+        Guitar ibanez = new Guitar();
+        ibanez.chords = 6;
+        ibanez.name = "Ibanez";
+    }
+}
